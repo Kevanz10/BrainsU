3efc52ac82115fb89a19fd44c1b53b9f96d0aaa0
Ubuntu <krojas1171@gmail.com>
2016-05-15 22:02:07 -0500

    Adding some extras about result page

--- brains/app/controllers/quiz_sessions_controller.rb
+++ brains/app/controllers/quiz_sessions_controller.rb
@@ -9,17 +9,26 @@ class QuizSessionsController < ApplicationController
   end
 
   def check
+    session[:test] ||= []
+    # Initialize var test and set the array if it's null.
+    
     session[:correct] ||= 0
     if params[:answer] == @quiz.questions[session[:count]].correcta.to_s
       session[:correct] += 1
     end
+    session[:test] << {:pregunta => @quiz.questions[session[:count]].id, 
+                      :restudiante => params[:answer]}
     session[:count] += 1
+
     @step = @quiz.questions[session[:count]]
+
     if @step.nil?
       redirect_to :action => "results" 
     else
       redirect_to :action => "questions" 
     end
+
+   
   end
 
   def results
