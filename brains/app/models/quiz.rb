5ed77ba2445a6dcdae3cd2116d5b162ab8ba3acb
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2016-05-20 02:02:05 -0500

    Add quiz_section model

--- brains/app/models/quiz.rb
+++ brains/app/models/quiz.rb
@@ -1,5 +1,7 @@
 class Quiz < ActiveRecord::Base
 	has_many :questions, :dependent => :destroy
+  has_many :quiz_sessions, :dependent => :destroy
+
 	accepts_nested_attributes_for :questions, allow_destroy: true,
 																reject_if: :all_blank
 
